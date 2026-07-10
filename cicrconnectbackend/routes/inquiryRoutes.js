397c320a56e79971e251597c33ff1bd894d6d300
Aman Raj <aman.raj.digiflute@gmail.com>
2026-07-10 22:40:19 +0530

    hotfix: use correct authorize middleware instead of restrictTo

--- cicrconnectbackend/routes/inquiryRoutes.js
+++ cicrconnectbackend/routes/inquiryRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const inquiryController = require('../controllers/inquiryController');
-const { protect, restrictTo } = require('../middleware/authMiddleware');
+const { protect, authorize } = require('../middleware/authMiddleware');
 
 router.post('/', inquiryController.submitInquiry);
-router.get('/', protect, restrictTo('Admin', 'Head'), inquiryController.getInquiries);
+router.get('/', protect, authorize('Admin', 'Head'), inquiryController.getInquiries);
 
 module.exports = router;
